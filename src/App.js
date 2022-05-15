237b63283d144af0460d1faddc3a4da6b6d86e1d
Emmanuel Salazar <emmanuel.salazar@avantica.net>
2022-05-15 10:52:57 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta es una prueba de como voy a probar React
         </p>
         <a
           className="App-link"
