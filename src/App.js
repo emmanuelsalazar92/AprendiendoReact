25f67b216a150637779df60550a355a6b6bb925f
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2022-05-15 13:10:38 -0600

    try 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Autodeploy prueba 2
+          Autodeploy prueba 3
         </p>
         <a
           className="App-link"
